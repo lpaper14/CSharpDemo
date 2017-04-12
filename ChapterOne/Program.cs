6abd78ea0da1b41fae7e5bac50019c7c78b2e9e4
Jacob Wang <JacobWang1029@gmail.com>
2017-04-12 23:09:20 +0800

    Init a C# project

---
+++ ChapterOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChapterOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ObsoleteMethod();
+        }
+        [Obsolete("This method is Obsolete!")]
+        public static void ObsoleteMethod()
+        {
+            Console.WriteLine("This method is obsolete!");
+        }
+    }
+
+    public class Winnebago
+    {
+        //public ulong notCompliant;
+    }
+}
---
+++ ChapterThree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChapterThree
+{
+    class Program
+    {
+        static void Main()
+        {
+            // if we use this method to get command line arguments, we don't even need the args as input parameters in Main().
+            var cmdArgs = Environment.GetCommandLineArgs();
+            foreach (var arg in cmdArgs)
+            {
+                Console.WriteLine($"Arg is: {arg}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
